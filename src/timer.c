d191451576f615abd6e8dc9e23513703d4fcb02e
suter <frederic.suter@cc.in2p3.fr>
2013-01-17 14:04:38 +0100

    add missing include and comments

--- src/timer.c
+++ src/timer.c
@@ -1,8 +1,12 @@
 #include "timer.h"
+#include <stdlib.h>
 
+/*
+ * Just a wrapper on gettimeofday to get time in the same format as SimGrid
+ */
 double getTime(){
-	struct timeval t;
-	gettimeofday(&t, NULL);
-	return (double) (t.tv_sec + t.tv_usec /1e6);
+  struct timeval t;
+  gettimeofday(&t, NULL);
+  return (double) (t.tv_sec + t.tv_usec /1e6);
 }
 
